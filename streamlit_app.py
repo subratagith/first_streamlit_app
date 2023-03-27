69b91afbed8988e7bdd3c21e430f1efa109d0af4
subratagith <128922405+subratagith@users.noreply.github.com>
2023-03-27 21:30:17 +0200

    Update streamlit_app.py
    aded 
    fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
    streamlit.write('The user entered ', fruit_choice)

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,5 +56,8 @@ streamlit.write('The user entered ', fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 streamlit.dataframe(fruityvice_response)
 
