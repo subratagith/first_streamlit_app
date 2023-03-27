f2d8a77545ee84b395e9c0dfa647d1ab3645820a
subratagith <128922405+subratagith@users.noreply.github.com>
2023-03-27 21:18:11 +0200

    Update streamlit_app.py
     removing the line streamlit.text(fruityvice_response.json()) to remove the json from the page

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ streamlit.dataframe(fruits_to_show)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())
 
 streamlit.header("Fruityvice Fruit Advice!")
 
