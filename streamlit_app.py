1d670cba010d6710227a5fae1deee3c943b25952
subratagith <128922405+subratagith@users.noreply.github.com>
2023-03-30 20:13:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,6 @@ import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
-
- 
-
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🍨Omega 3 and Blueberry Oatmeal')
