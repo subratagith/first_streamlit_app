9a4ffd5e5c0e6f31b904d2cff37aaf6dc128058e
subratagith <128922405+subratagith@users.noreply.github.com>
2023-03-31 07:44:59 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get response")
-    #else:
+    else:
       back_form_function = get_fruityvice_data(fruit_choice)
       streamlit.dataframe(back_form_function)
       except URLError as e:
